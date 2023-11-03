02d24fc6de9162482047d9b2733c2440f3396a78
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2023-11-03 17:02:42 +0530

    Update random.js

--- random.js
+++ random.js
@@ -1,6 +1,14 @@
 function getRandomName() {
-  const firstName = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank"];
-  const lastName = ["Smith", "Johnson", "Williams", "Brown", "Davis", "Miller"];
+  const firstName = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank",
+  "Grace", "Hannah", "Isaac", "Jack", "Katherine", "Liam",
+  "Olivia", "Noah", "Sophia", "Michael", "Elizabeth", "William",
+  "Ava", "James", "Mia", "John", "Ella", "Robert",
+  "Emily", "Joseph", "Sofia", "Daniel", "Chloe", "Matthew"];
+  const lastName = ["Smith", "Johnson", "Williams", "Brown", "Davis", "Miller",
+  "Wilson", "Moore", "Lee", "Taylor", "Anderson", "Thomas",
+  "Martinez", "Harris", "Martin", "Jackson", "Garcia", "Jones",
+  "Lopez", "Clark", "White", "Rodriguez", "Lewis", "Hall",
+  "Walker", "Young", "King", "Hill", "Scott", "Adams"];
   
   const randomFirstName = firstName[Math.floor(Math.random() * firstName.length)];
   const randomLastName = lastName[Math.floor(Math.random() * lastName.length)];
