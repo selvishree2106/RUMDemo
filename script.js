9139af9ecbc5b34d27f22168b85c31532c6407e7
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2023-11-03 16:16:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,13 +33,3 @@ menuBtn.addEventListener('click', ()=>{
         }
     })
 })
-
-function getRandomName() {
-  const firstName = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank"];
-  const lastName = ["Smith", "Johnson", "Williams", "Brown", "Davis", "Miller"];
-  
-  const randomFirstName = firstName[Math.floor(Math.random() * firstName.length)];
-  const randomLastName = lastName[Math.floor(Math.random() * lastName.length)];
-  
-  return `${randomFirstName} ${randomLastName}`;
-}
